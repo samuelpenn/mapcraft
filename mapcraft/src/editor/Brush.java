d77826664f2e27444002a0ed1ae7f51ebaa66bfb
sam <sam@f5154be7-dd40-0410-84e4-b074b930c8e3>
2002-09-26 20:23:04 +0000

    Added.
    git-svn-id: http://svn.code.sf.net/p/mapcraft/code/trunk@9 f5154be7-dd40-0410-84e4-b074b930c8e3

---
+++ mapcraft/src/editor/Brush.java
@@ -0,0 +1,76 @@
+package uk.co.demon.bifrost.rpg.xmlmap;
+
+
+/**
+ * The Brush class provides support for a 'brush' - the concept
+ * used for painting to the map. A brush has a type (which may
+ * be changed by the user), which defines which layer of the map
+ * is painted to, and a selection, which is the type of object
+ * painted.
+ */
+class Brush {
+    public static final int NULL = 0;
+    public static final int TERRAIN = 1;
+    public static final int SITES = 2;
+    public static final int RIVERS = 3;
+    public static final int HILLS = 4;
+    public static final int HEIGHT = 5;
+
+    private int brush = TERRAIN;
+
+    private short terrain = 1;
+    private short river = 1;
+    private short site = 0;
+    private short hill = 0;
+    private short height = 0;
+
+    Brush() {
+        brush = TERRAIN;
+    }
+
+    int
+    getType() {
+        return brush;
+    }
+
+    short
+    getSelected() {
+        switch (brush) {
+        case TERRAIN: return terrain;
+        case SITES:   return site;
+        case RIVERS:  return river;
+        case HILLS:   return hill;
+        case HEIGHT:  return height;
+        default:
+            return 0;
+        }
+    }
+
+    void
+    setType(int type) {
+        this.brush = type;
+    }
+
+    void
+    setSelected(int type, short selected) {
+        switch (type) {
+        case TERRAIN:
+            terrain = selected;
+            break;
+        case SITES:
+            site = selected;
+            break;
+        case RIVERS:
+            river = selected;
+            break;
+        case HILLS:
+            hill = selected;
+            break;
+        case HEIGHT:
+            height = selected;
+            break;
+        default:
+            return;
+        }
+    }
+}
