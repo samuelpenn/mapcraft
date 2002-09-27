7ee80ff8cc935b939684f27e5ae020e458009a23
sam <sam@f5154be7-dd40-0410-84e4-b074b930c8e3>
2002-09-27 21:52:49 +0000

    Added gridbag layout. Still needs Okay button and ability to grab info from it.
    git-svn-id: http://svn.code.sf.net/p/mapcraft/code/trunk@11 f5154be7-dd40-0410-84e4-b074b930c8e3

--- mapcraft/src/editor/SiteDialog.java
+++ mapcraft/src/editor/SiteDialog.java
@@ -6,10 +6,63 @@ import java.awt.*;
 import java.awt.image.*;
 
 public class SiteDialog extends JDialog {
-    Site    site;
+    private Site        site;
+    private JTextField  name;
+    private JTextField  description;
+    
+    public String
+    getName() {
+        if (name != null) {
+            return name.getText();
+        }
+        
+        return "";
+    }
     
     public
     SiteDialog(Site site) {
+        super();
         this.site = site;
+        
+        GridBagLayout       gridbag = new GridBagLayout();
+        GridBagConstraints  c = new GridBagConstraints();
+        JLabel              label = null;
+        getContentPane().setLayout(gridbag);
+
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = c.weighty = 1.0;
+
+        name = new JTextField("Unnamed", 20);
+        description = new JTextField("Unknown", 30);
+
+        label = new JLabel("Name");
+        c.gridx = 0;
+        c.gridy = 0;
+        gridbag.setConstraints(label, c);
+        getContentPane().add(label);
+
+        c.gridx = 1;
+        gridbag.setConstraints(name, c);
+        getContentPane().add(name);
+
+        c.gridx=0;
+        c.gridy=1;
+        label = new JLabel("Description");
+        gridbag.setConstraints(label, c);
+        getContentPane().add(label);
+
+        c.gridx = 1;
+        gridbag.setConstraints(description, c);
+        getContentPane().add(description);
+
+
+        setSize(new Dimension(300,150));
+        setVisible(true);
+    }
+    
+    public static void
+    main(String args[]) {
+        Site        site = new Site((short)1, "London", "Large city");
+        SiteDialog  dialog = new SiteDialog(site);
     }
 }
