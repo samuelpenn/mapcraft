0bf0dba40b05f9c8f246ceb0631bbae5dbdfbc70
sam <sam@f5154be7-dd40-0410-84e4-b074b930c8e3>
2003-08-31 16:10:41 +0000

    Crop to hex now works.
    git-svn-id: http://svn.code.sf.net/p/mapcraft/code/trunk@145 f5154be7-dd40-0410-84e4-b074b930c8e3

--- mapcraft/test/Show.java
+++ mapcraft/test/Show.java
@@ -15,14 +15,57 @@ import com.sun.image.codec.jpeg.*;
 public class Show implements ImageObserver {
     public static Toolkit toolkit = Toolkit.getDefaultToolkit();
     public boolean gotImage = false;
+
+    public class
+    HexFilter extends RGBImageFilter {
+        private int     width;
+        private int     height;
+        private int     h;
+        private double  SQRTHREE;
+
+        public
+        HexFilter(int width, int height) {
+            this.width = width;
+            this.height = height;
+
+            canFilterIndexColorModel = true;
+
+            SQRTHREE = Math.sqrt(3);
+            h = (int)((SQRTHREE/4)*width);
+            System.out.println(SQRTHREE);
+        }
+
+        public int
+        filterRGB(int x, int y, int rgb) {
+            int     dx = x;
+            int     dy = y;
+
+            if (y > h*2) {
+                return 0;
+            }
+
+            if (x > width/2) {
+                dx = width - x;
+            }
+            if (y > h) {
+                dy = (h*2) - y;
+            }
+
+            int  a = (int)(SQRTHREE*dx);
+            if ((h-dy) > a) {
+                rgb = 0;
+            }
+
+            return rgb;
+        }
+    }
+
     public
     Show() {
     }
 
     public boolean
     imageUpdate(Image img, int infoflags, int x, int y, int w, int h) {
-        System.out.println("Available: "+x+","+y+","+w+","+h);
-
         if (h == 96) gotImage = true;
 
         return true;
@@ -41,12 +84,16 @@ public class Show implements ImageObserver {
             } catch (Exception e) {}
         } while (!gotImage);
 
+        HexFilter  filter = new HexFilter(96, 96);
+        draw = frame.createImage(new FilteredImageSource(image.getSource(), filter));
+
+
 //        System.out.println(g.drawImage(image, 0, 0, this));
 
-        g.drawImage(image, 0, 0, this);
+        //g.drawImage(image, 0, 0, this);
 
-        g.setBackground(new Color(255, 255, 255, 255));
-        g.clearRect(0, 0, 75, 25);
+        //g.setBackground(new Color(255, 255, 255, 255));
+        //g.clearRect(0, 0, 75, 25);
 
         //g.setPaintMode();
         g.setColor(new Color(0,0,0,255));
