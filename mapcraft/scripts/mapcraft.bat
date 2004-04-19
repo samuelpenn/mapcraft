3e7dd729b9520803d7e8b7bbeca453d00d7664cc
samuelpenn <samuelpenn@f5154be7-dd40-0410-84e4-b074b930c8e3>
2004-04-19 20:36:21 +0000

    Tested on work PC. Should be DOS format now. $* changed to %*.
    git-svn-id: http://svn.code.sf.net/p/mapcraft/code/trunk@278 f5154be7-dd40-0410-84e4-b074b930c8e3

--- mapcraft/scripts/mapcraft.bat
+++ mapcraft/scripts/mapcraft.bat
@@ -1,14 +1,23 @@
 rem Startup script for MS Windows
-rem Totally untested since I don't have an MS Windows box to test it on.
+
+rem Tested briefly and seems to work
+rem Remember to set the XALAN_HOME variable before running
+
 
 rem Version: $Revision$
 rem Date:    $Date$
+
 rem Author:  Samuel Penn
 
-set XALAN_HOME=C:\share\xalan
+
+set XALAN_HOME=C:\share\libs
+
 set MAPCRAFT_JAR=mapcraft.jar
+
 
 set CLASSPATH=%XALAN_HOME%\xalan.jar;%XALAN_HOME%\xercesImpl.jar;%MAPCRAFT_JAR%
 
-java -Xmx1024m uk.co.demon.bifrost.rpg.mapcraft.MapCraft $*
+
+
+java -Xmx1024m uk.co.demon.bifrost.rpg.mapcraft.MapCraft %*
 
