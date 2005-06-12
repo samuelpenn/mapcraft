8620a951d02f7478b246370b47c7734dc681adf0
samuelpenn <samuelpenn@f5154be7-dd40-0410-84e4-b074b930c8e3>
2005-06-12 19:00:43 +0000

    Email address change.
    git-svn-id: http://svn.code.sf.net/p/mapcraft/code/trunk@339 f5154be7-dd40-0410-84e4-b074b930c8e3

--- mapcraft/src/uk/co/demon/bifrost/utils/Options.java
+++ mapcraft/src/uk/co/demon/bifrost/utils/Options.java
@@ -1,12 +1,12 @@
 /*
- * Copyright (C) 2002 Samuel Penn, sam@bifrost.demon.co.uk
+ * Copyright (C) 2002 Samuel Penn, sam@glendale.org.uk
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2,
  * or (at your option) any later version. See the file COPYING.
  */
-package uk.co.demon.bifrost.utils;
+package net.sourceforge.mapcraft.uk.co.demon.bifrost.utils;
 
 
 /**
