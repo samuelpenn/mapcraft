ab3fb93bac243e47c6688af7f6b3011dbf98472f
samuelpenn <samuelpenn@f5154be7-dd40-0410-84e4-b074b930c8e3>
2009-01-04 23:09:01 +0000

    Display market info on a planet.
    git-svn-id: http://svn.code.sf.net/p/mapcraft/code/trunk@432 f5154be7-dd40-0410-84e4-b074b930c8e3

---
+++ traveller/src/uk/org/glendale/rpg/traveller/servlets/Market.java
@@ -0,0 +1,177 @@
+/*
+ * Copyright (C) 2009 Samuel Penn, sam@glendale.org.uk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation version 2.
+ * See the file COPYING.
+ */
+package uk.org.glendale.rpg.traveller.servlets;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.util.Hashtable;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import uk.org.glendale.rpg.traveller.civilisation.trade.Commodity;
+import uk.org.glendale.rpg.traveller.civilisation.trade.Trade;
+import uk.org.glendale.rpg.traveller.civilisation.trade.TradeGood;
+import uk.org.glendale.rpg.traveller.database.ObjectFactory;
+import uk.org.glendale.rpg.traveller.database.ObjectNotFoundException;
+import uk.org.glendale.rpg.traveller.systems.Planet;
+
+/**
+ * A RESTful implementation of a web service that enables access to market
+ * information about a planet.
+ * 
+ * A request should always be of the format /market/<planetId>. Individual
+ * methods will define further requirements.
+ * 
+ * @author Samuel Penn
+ * @created 2009/01/3
+ *
+ */
+public class Market extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Get information on a resource. By default, an XML description of the
+	 * planet is returned, however the format can be modified by appending a
+	 * filename extension to the id, e.g.:
+	 *   /market/6801.html   An HTML page describing the planet.
+	 *   /market/6801.txt    Return XML data as content type text.
+	 *   
+	 * For specific data, the txt format returns a simple text string.
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String		uri = request.getPathInfo();
+		String		format = "xml";
+		int			id = 0;
+		
+		// Very quick test to ensure that the URL format is correct.
+		if (!uri.matches("/[0-9]+([/.].*)?")) {
+			response.sendError(400, "Incorrect request format ["+uri+"]");
+			return;
+		}
+		
+		try {
+			id = Integer.parseInt(uri.replaceAll("/([0-9]+)([/.].*)?", "$1"));
+		} catch (NumberFormatException e) {
+			// Should be impossible, since the regexp will have failed by this point.
+			response.sendError(400, "Badly formatted planet id");
+			return;
+		}
+
+		if (uri.matches(".*\\.[a-z]+")) {
+			format = uri.replaceAll(".*\\.([a-z]+)", "$1");
+			if (format.equals("txt") || format.equals("xml") || format.equals("html") || format.equals("jpg")) {
+				// Okay.
+			} else {
+				response.sendError(415, "Unrecognised format type ["+format+"]");
+				return;
+			}
+		}
+
+		try {
+			ObjectFactory		factory = new ObjectFactory();
+			Planet				planet = factory.getPlanet(id);
+
+			getFullPage(factory, planet, format, request, response);
+		} catch (ObjectNotFoundException e) {
+			response.sendError(404, "Cannot find planet with id ["+id+"]");
+			return;
+		} catch (Throwable t) {
+			t.printStackTrace();
+			response.sendError(500, "Exception ("+t.getMessage()+")");
+			return;
+		}
+	}
+	
+	/**
+	 * Output a full page of data to the client. The format of tha data will
+	 * depend on the format requested.
+	 * 
+	 * @param factory		ObjectFactory giving access to the universe.
+	 * @param planet		Planet to get information on.
+	 * @param format		Format of the response.
+	 * @param request		Request object.
+	 * @param response		Response object.
+	 * @throws IOException
+	 */
+	private void getFullPage(ObjectFactory factory, Planet planet, String format, HttpServletRequest request, HttpServletResponse response) throws IOException {
+		if (format.equals("html")) {
+			// Output information as HTML.
+			response.setContentType("text/html");
+			response.getOutputStream().print(outputHTML(factory, planet, request.getContextPath()));
+		} else if (format.equals("txt")) {
+			// Not currently supported.
+		} else {
+			// Not currently supported.
+		}
+	}
+
+	/**
+	 * Output information about the planet as HTML. Also adds HTML header
+	 * and footer since the raw planet HTML is only an HTML snippet.
+	 * 
+	 * @param planet	Planet to output data on.
+	 * @return			String containing the HTML page.
+	 */
+	private String outputHTML(ObjectFactory factory, Planet planet, String contextPath) {
+		StringBuffer		buffer = new StringBuffer();
+		String				stylesheet = contextPath+"/data/css/system.css";
+		String				imageBase = contextPath+"/data/images/trade/";
+		Trade				trade = new Trade(factory, planet);
+		
+		buffer.append("<html>\n<head>\n<title>"+planet.getName()+"</title>\n");
+		buffer.append("<link rel=\"STYLESHEET\" type=\"text/css\" media=\"screen\" href=\""+stylesheet+"\" />\n");
+		buffer.append("</head><body>\n");
+		
+		buffer.append("<div class=\"header\">");
+		buffer.append("<h1>"+planet.getName()+"</h1>");
+		buffer.append("</div>");
+		
+		buffer.append("<div class=\"planet\">");
+		buffer.append("<p>");
+		buffer.append("<b>Planet Type: </b>"+planet.getType()+"; ");
+		buffer.append("<b>Population: </b>"+planet.getPopulation()+"; ");
+		buffer.append("<b>Government: </b>"+planet.getGovernment()+"; ");
+		buffer.append("<b>Tech Level: </b>"+planet.getTechLevel()+"; ");
+		buffer.append("<b>Star port: </b>"+planet.getStarport()+"; ");		
+		buffer.append("</p>");
+		
+		buffer.append("<p>");
+		buffer.append("<b>Production Capacity: </b>"+trade.getProductionCapacity()+"; ");
+		buffer.append("</p>");
+		buffer.append("</div>");
+		
+		buffer.append("<div class=\"marketdata\">");
+		Hashtable<Integer,TradeGood>	goods = factory.getCommoditiesByPlanet(planet.getId());
+		
+		for (int i : goods.keySet()) {
+			TradeGood		good = goods.get(i);
+			Commodity		c = factory.getCommodity(good.getCommodityId());
+			
+			buffer.append("<div class=\"good\">");
+			buffer.append("<img src=\""+imageBase+c.getImage()+".png\" align=\"left\"/>");
+			buffer.append("<h4>"+c.getName()+"</h4>");
+			buffer.append("<p><b>Amount:</b> "+good.getAmount()+" @ "+good.getPrice()+"Cr; ");
+			buffer.append("<b>Demand: </b>"+trade.getLocalDemand(c)+"/wk; ");
+			buffer.append("<b>Production: </b>"+trade.getProductionRate(c)+"/wk");
+			buffer.append("</p>");
+			buffer.append("</div>");
+		}
+		buffer.append("</div>");
+		
+		buffer.append("</body></html>\n");
+		
+		return buffer.toString();
+	}	
+}
