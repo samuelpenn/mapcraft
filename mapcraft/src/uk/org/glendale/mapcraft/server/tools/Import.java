86ffff5cb2cb500a1f13319ca093ba72600af9ac
samuelpenn <samuelpenn@f5154be7-dd40-0410-84e4-b074b930c8e3>
2009-06-27 16:11:10 +0000

    Added first part of new project.
    git-svn-id: http://svn.code.sf.net/p/mapcraft/code/trunk@442 f5154be7-dd40-0410-84e4-b074b930c8e3

---
+++ mapcraft/src/uk/org/glendale/mapcraft/server/tools/Import.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2009 Samuel Penn, sam@glendale.org.uk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2.
+ */
+package uk.org.glendale.mapcraft.server.tools;
+
+/**
+ * Imports an XML map file from earlier versions of Mapcraft. 
+ * 
+ * @author Samuel Penn
+ */
+public class Import {
+
+}
