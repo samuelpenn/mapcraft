f3df76acc980f6e9563b15a14d5392e4fa78a504
samuelpenn <samuelpenn@f5154be7-dd40-0410-84e4-b074b930c8e3>
2011-10-30 21:09:27 +0000

    Started work on FacilityGenerator to allow it to randomly generate facilities for a world. Extended list of possible facilities and the commodities that they require.
    git-svn-id: http://svn.code.sf.net/p/mapcraft/code/trunk@634 f5154be7-dd40-0410-84e4-b074b930c8e3

---
+++ traveller/src/main/java/uk/org/glendale/worldgen/astro/planet/PopulationSize.java
@@ -0,0 +1,16 @@
+package uk.org.glendale.worldgen.astro.planet;
+
+public enum PopulationSize {
+	/** Up to 100 people. */
+	Tiny,
+	/** Up to 10,000 people. */
+	Small,
+	/** Up to 1 million people. */
+	Medium,
+	/** Up to 100 million people. */
+	Large,
+	/** Up to 10 billion people. A full civilisation. */
+	Huge,
+	/** More than 10 billion people. City or Hive worlds. */
+	Gigantic
+}
