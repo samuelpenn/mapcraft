74d98c5133955d6a87a8ccb665d6d5584a2a355d
samuelpenn <samuelpenn@f5154be7-dd40-0410-84e4-b074b930c8e3>
2012-08-31 23:16:49 +0000

    Improve details of Europan worlds.
    git-svn-id: http://svn.code.sf.net/p/mapcraft/code/trunk@698 f5154be7-dd40-0410-84e4-b074b930c8e3

--- traveller/src/main/java/uk/org/glendale/worldgen/astro/planet/builders/ice/Europan.java
+++ traveller/src/main/java/uk/org/glendale/worldgen/astro/planet/builders/ice/Europan.java
@@ -37,22 +37,33 @@ public class Europan extends IceWorld {
 		planet.setRadius(radius / 2 + Die.die(radius, 2)/2);
 		if (planet.getRadius() > 3000) {
 			planet.setPressure(AtmospherePressure.Trace);
-			planet.setAtmosphere(AtmosphereType.InertGases);
+			switch (Die.d3()) {
+			case 1:
+				planet.setAtmosphere(AtmosphereType.Oxygen);
+				break;
+			case 2:
+				planet.setAtmosphere(AtmosphereType.WaterVapour);
+				break;
+			case 3:
+				planet.setAtmosphere(AtmosphereType.InertGases);
+				break;
+			}
 			planet.addTradeCode(TradeCode.Ba);
 		} else {
+			planet.setPressure(AtmospherePressure.None);
 			planet.addTradeCode(TradeCode.Va);			
 		}
 		planet.addTradeCode(TradeCode.H3);
 		
 		// Work out ecology, if any.
-		switch (Die.d6(3)) {
-		case 3:
+		switch (Die.d6(2)) {
+		case 2:
 			planet.setLifeType(LifeType.Aerobic);
 			break;
-		case 4: case 5:
+		case 3: case 4:
 			planet.setLifeType(LifeType.Archaean);
 			break;
-		case 6: case 7: case 8:
+		case 5: case 6: case 7:
 			planet.setLifeType(LifeType.Organic);
 			break;
 		}
@@ -69,6 +80,36 @@ public class Europan extends IceWorld {
 	@Override
 	public void generateResources() {
 		addResource("Water", 20 + Die.d20(4));
+		
+		int level = 0;
+		switch (planet.getLifeType()) {
+		case Organic:
+			addResource("Organic Chemicals", Die.d20());
+			break;
+		case Archaean:
+			level = Die.d10() + 10;
+			addResource("Organic Chemicals", level);
+			switch (Die.d3()) {
+			case 1:
+				addResource("Protobionts", level / Die.d3() + 1);
+				break;
+			case 2:
+				addResource("Protobionts", level / 2);
+				addResource("Cyanobacteria", level / 3);
+				break;
+			case 3:
+				addResource("Protobionts", level / 2);
+				addResource("Cyanobacteria", level + Die.d6());
+				break;
+			}
+			break;
+		case Aerobic:
+			level = 12 + Die.d6(3);
+			addResource("Cyanobacteria", level + Die.d6());
+			addResource("Algae", level + Die.d4() - Die.d6());
+			addResource("Cnidarians", level - Die.d6(2));
+			break;
+		}
 	}
 
 }
